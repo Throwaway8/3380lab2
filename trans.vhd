87bd45186314008b0975894af432d1b6610abe4e
Thom <44063708+Throwaway8@users.noreply.github.com>
2019-03-16 17:56:05 -0700

    Update trans.vhd

--- trans.vhd
+++ trans.vhd
@@ -13,9 +13,9 @@ end trans;
 architecture char of trans is
 
 signal state: integer range 0 to 2;
-signal count: integer range 0 to 19;
+signal count: integer range 0 to 20;
 
-signal m2: std_logic_vector(18 downto 0);
+signal m2: std_logic_vector(19 downto 0);
 
 begin
 
@@ -29,19 +29,20 @@ begin
 		
 			case state is 
 				when  0 =>
-					if load = '0' and count = 0 then
+					if load = '0' then
+						s <= '0';
 						tx <= '0';
 						state <= 0 after 1000000000 ns;
-					elsif load = '1' and count = 0 then
+					elsif load = '1'then
 							tx <= '0';
-							m2(0) <= m(0) xor m(1) xor m(3) xor m(4) xor m(6) xor m(8) xor m(10) xor m(11) xor m(13);
-							m2(1) <= m(0) xor m(2) xor m(3) xor m(5) xor m(6) xor m(9) xor m(10) xor m(12) xor m(13);
+							m2(0) <= m(2) xor m(4) xor m(6) xor m(8) xor m(10) xor m(12);
+							m2(1) <= m(2) xor m(5) xor m(6) xor m(9) xor m(10) xor m(13);
 							m2(2) <= m(0);
-							m2(3) <= m(1) xor m(2) xor m(3) xor m(7) xor m(8) xor m(9) xor m(10);
+							m2(3) <= m(4) xor m(5) xor m(6) xor m(11) xor m(12) xor m(13);
 							m2(4) <= m(1);
 							m2(5) <= m(2);
 							m2(6) <= m(3);
-							m2(7) <= m(4) xor m(5) xor m(6) xor m(7) xor m(8) xor m(9) xor m(10);
+							m2(7) <= m(8) xor m(9) xor m(10) xor m(11) xor m(12) xor m(13);
 							m2(8) <= m(4);
 							m2(9) <= m(5);
 							m2(10) <= m(6);
@@ -53,25 +54,28 @@ begin
 							m2(16) <= m(11);
 							m2(17) <= m(12);
 							m2(18) <= m(13);
-						state <= 1 after 1000000000 ns;
+							m2(19) <= m2(0) xor m2(1) xor m2(2) xor m2(3) xor m2(4) xor m2(5) xor m2(6) xor m2(7) xor m2(8) xor m2(9)
+							xor m2(10) xor m2(11) xor m2(12) xor m2(13) xor m2(14) xor m2(15) xor m2(16) xor m2(17) xor m2(18) xor m2(19);
+							state <= 1 after 1000000000 ns;
 					end if;
 				
 				when 1 =>
-					if load = '1' and count = 0 then 
+					if load = '1' then 
+						tx <= '0';
 						state <= 1 after 1000000000 ns;
-					elsif load = '0' and count = 0 then
+					elsif load = '0' then
 						tx <= '1';
 						state <= 2 after 1000000000 ns;
 					end if;
 				
 				when 2 =>
-					if load = '0' and count <= 19 then
+					if load = '0' and count <= 20 then
 						tx <= '1';
-						s <= m2(18);
-						m2 <= m2(17 downto 0) & '0';
+						s <= m2(19);
+						m2(19 downto 0) <= m2(18 downto 0) & '0';
 						count <= count + 1;
 						state <= 2 after 1000000000 ns;
-					elsif load = '0' and count > 19 then
+					elsif load = '0' and count > 20 then
 						tx <= '0';
 						state <= 0 after 1000000000 ns;
 					end if;
